a964c569df103b14c498aa451eafa9d33960ff67
leonard <leonard@virtuoso@gmail.com>
2021-10-21 08:56:47 +0300

    project eats start

---
+++ Components/HeaderTabs.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { View, Text, TouchableOpacity } from 'react-native'
+
+export default function HeaderTabs() {
+    return (
+        <View style={{flexDirection:'row',alignSelf:'center'}}>
+           <HeaderButton text="Delivery" btnColor="black" textColor="white"/>
+           <HeaderButton text="Pickup" btnColor="white" textColor="black"/>
+        </View>
+    )
+}
+const HeaderButton = (props) => (
+
+<TouchableOpacity style={{backgroundColor: props.btnColor,
+                          paddingVertical:6, 
+                         paddingHorizontal:16,
+                         borderRadius:30,
+                         marginTop: 20,
+                         }}>
+   <Text style={{color:props.textColor, fontSize:15,fontWeight:"900"}}>{props.text}</Text>
+ </TouchableOpacity>
+
+);
---
+++ Screens/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text, SafeAreaView } from 'react-native'
+import HeaderTabs from '../Components/HeaderTabs'
+
+export default function Home() {
+    return (
+     <SafeAreaView>
+           <HeaderTabs/>
+     </SafeAreaView>
+
+    );
+}
